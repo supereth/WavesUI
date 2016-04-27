3a7d63aec699d49ddd01f9737582d71bca27bbf6
eth <eth@supernet.org>
2016-04-27 12:45:59 +0800

    Responsiveness and other improvements.
    Several javascript changes on element on-screen resize/distribution for
    improved performance.
    Re-adjusted case in where version number was overlapping main logo.
    Adjusted PREFERENCES/PROFILE icons for mobile versions.
    Re-adjusted Balance Boxes button sets.
    Added responsiveness for 420px width and below mobile-screens in
    portrait mode for WALLET tab.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     // Show/hide different sections on tab activation
     $('input[type=radio]').click(function(){
 
-        $('.mBB-content, .LBmBB-content').fadeOut(200);
+        $('.mBB-content, .LBmBB-content').fadeOut(200).delay(400);
         $('#' + $(this).val()).fadeIn(800);
         $('#LB' + $(this).val()).fadeIn(800);
 
@@ -17,14 +17,6 @@ $(document).ready(function(){
             case 'mBB-portfolio':
                 loadPayment();
             break;
-        }
-        
-        // Displays left bar depending on tab activated
-        if ($('input[type=radio][name=tabs-Icons]:checked').val() == 'mBB-wallet' || $('input[type=radio][name=tabs-Icons]:checked').val() == 'mBB-history') {
-            $('#mBLeftBar').css('display', 'none');
-            $('#mBB-wallet').css('text-align', 'center');
-        } else {
-            $('#mBLeftBar').css('display', 'table-cell');
         }
     });
 
