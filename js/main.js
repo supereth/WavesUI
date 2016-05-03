d1ad5ebf7c3cd6b29e17f754969358470476eb65
Tosch110 <tosch110@zedat.fu-berlin.de>
2016-05-03 06:33:27 +0200

    Port functions
    Porting functions to new framework

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
 "use strict";
 
- initApp();
-
-$(document).ready(function(){
-
-    getKeyPair('123');
-
-    // Show/hide different sections on tab activation
-    $('input[type=radio]').click(function(){
-
-        $('.mBB-content, .LBmBB-content').fadeOut(200).delay(400);
-        $('#' + $(this).val()).fadeIn(800);
-        $('#LB' + $(this).val()).fadeIn(800);
-
-        var linkType = $(this).val();
-
-        switch(linkType) {
-            case 'mBB-portfolio':
-                loadPayment();
-            break;
-        }
-    });
-
-});
-
-
-
-function initApp() {
-    loadHistory();
-    loadBlockheight();
-    loadBalance();
-}
+ 
